aacaf96affafca73b68efd55dfe826d0e70b3a76
Menaka Anton <menakaantonjayakody@gmail.com>
2022-11-13 15:04:54 +0530

    step 01 done

--- public/sketch.js
+++ public/sketch.js
@@ -6,6 +6,14 @@ function setup() {
   background(51);
 
   socket =io.connect('http://localhost:3000');
+  socket.on('mouse',newDrawing);
+
+}
+
+function newDrawing(data){
+  noStroke();
+  fill(255,0,100);
+  ellipse(data.x,data.y,60,60);
 }
 
 function mouseDragged(){
