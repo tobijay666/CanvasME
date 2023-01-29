510e9eb2440b11af49e0b10c78041b82c3d63d28
Menaka Anton <menakaantonjayakody@gmail.com>
2023-01-29 21:43:06 +0530

    Tests Updated

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -21,3 +21,40 @@ describe('LoginComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+describe('Login form', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/login');
+  });
+
+  it('displays the login form', () => {
+    cy.get('form').should('be.visible');
+    cy.get('input[name="email"]').should('be.visible');
+    cy.get('input[name="password"]').should('be.visible');
+    cy.get('button[type="submit"]').should('be.visible');
+  });
+
+  it('submits the form with valid input', () => {
+    cy.get('input[name="email"]').type('test@example.com');
+    cy.get('input[name="password"]').type('password123');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.success-message').should('be.visible').and('contain', 'Logged In!');
+  });
+
+  it('displays error message with invalid input', () => {
+    cy.get('input[name="email"]').type('test@example.com');
+    cy.get('input[name="password"]').type('pass');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.validation-message').should('be.visible').and('contain', 'at least 6 characters');
+  });
+
+  it('displays error message for incorrect email/password', () => {
+    cy.get('input[name="email"]').type('wrong@example.com');
+    cy.get('input[name="password"]').type('wrongpassword');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.validation-message').should('be.visible').and('contain', 'Incorrect E-mail/Password');
+  });
+});
--- src/app/auth/sign-up/sign-up.component.spec.ts
+++ src/app/auth/sign-up/sign-up.component.spec.ts
@@ -21,3 +21,51 @@ describe('SignUpComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+describe('Sign Up Form', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/signup');
+  });
+
+  it('displays the sign up form', () => {
+    cy.get('form').should('exist');
+    cy.get('input[name="uname"]').should('exist');
+    cy.get('input[name="email"]').should('exist');
+    cy.get('input[name="password"]').should('exist');
+    cy.get('button[type="submit"]').should('exist');
+  });
+
+  it('submits the form and displays success message', () => {
+    cy.get('input[name="uname"]').type('testuser');
+    cy.get('input[name="email"]').type('testuser@example.com');
+    cy.get('input[name="password"]').type('password123');
+    cy.get('button[type="submit"]').click();
+    cy.get('.success-message').should('exist');
+    cy.get('.success-message').should('contain', 'Account Created!');
+  });
+
+  it('displays an error message when the form is submitted without a username', () => {
+    cy.get('input[name="email"]').type('testuser@example.com');
+    cy.get('input[name="password"]').type('password123');
+    cy.get('button[type="submit"]').click();
+    cy.get('.validation-message').should('exist');
+    cy.get('.validation-message').should('contain', 'This field is required');
+  });
+
+  it('displays an error message when the form is submitted without a valid email', () => {
+    cy.get('input[name="uname"]').type('testuser');
+    cy.get('input[name="email"]').type('invalidemail');
+    cy.get('input[name="password"]').type('password123');
+    cy.get('button[type="submit"]').click();
+    cy.get('.validation-message').should('exist');
+    cy.get('.validation-message').should('contain', 'Incorrect E-mail address');
+  });
+
+  it('displays an error message when the form is submitted without a password', () => {
+    cy.get('input[name="uname"]').type('testuser');
+    cy.get('input[name="email"]').type('testuser@example.com');
+    cy.get('button[type="submit"]').click();
+    cy.get('.validation-message').should('exist');
+    cy.get('.validation-message').should('contain', 'This field is required');
+  });
+});
